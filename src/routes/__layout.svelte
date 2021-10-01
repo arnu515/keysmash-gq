ec0118e9c64b11474b7679d9620732bd86875e0c
arnu515 <arnu515@protonmail.com>
2021-10-01 21:51:24 +0530

    feat(profile): Check for user profiles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,20 +9,33 @@ import { onMount } from "svelte";
 import session from "$lib/stores/session";
 import user from "$lib/stores/user";
 import Notifications from "$lib/components/Notifications.svelte";
+import profile, { getProfile } from "$lib/stores/profile";
+import { goto } from "$app/navigation";
 
 let loading = true;
 
-onMount(() => {
+onMount(async () => {
   $session = supabase.auth.session();
   $user = $session?.user;
 
-  supabase.auth.onAuthStateChange((_, ssn) => {
+  supabase.auth.onAuthStateChange(async (_, ssn) => {
     $session = ssn;
     $user = ssn?.user;
+    if ($user) {
+      $profile = await getProfile();
+    }
   });
+
+  if ($user) {
+    $profile = await getProfile();
+  }
 
   loading = false;
 });
+
+$: console.log($profile, $user);
+$: if (!loading && $user && !$profile && typeof window !== "undefined")
+  window.location.pathname.startsWith("/auth") || goto("/auth");
 </script>
 
 {#if !loading}
