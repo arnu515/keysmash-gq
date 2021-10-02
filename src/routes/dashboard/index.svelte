988399d88e2f96206544b1213c77dd865612d2bd
arnu515 <arnu515@protonmail.com>
2021-10-02 09:56:23 +0530

    feat: add dashboard

---
+++ src/routes/dashboard/index.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+import { onMount } from "svelte";
+import { getTeacher, Teacher } from "$lib/stores/profile";
+import BecomeTeacher from "$lib/components/BecomeTeacher.svelte";
+
+let teacher: Teacher;
+let becomeTeacherModalVisible = false;
+
+onMount(async () => {
+  teacher = await getTeacher();
+});
+</script>
+
+<h1 class="text-center text-5xl m-7 font-bold">Dashboard</h1>
+
+<main class="grid grid-cols-1 md:grid-cols-2 gap-4">
+  <section class="grid-section">
+    <h2 class="heading">Courses you're learning from</h2>
+  </section>
+  <section class="grid-section">
+    <h2 class="heading">Courses you're teaching</h2>
+    {#if !teacher}
+      <p class="text-center m-4">
+        You're not a teacher yet!
+        <button
+          class="text-secondary hover:text-secondary-hover cursor-pointer"
+          style="transition: color 500ms ease"
+          on:click={() => (becomeTeacherModalVisible = true)}
+        >
+          Become a teacher
+        </button>
+      </p>
+    {:else}
+      <p class="text-center m-4">
+        <a
+          class="text-secondary hover:text-secondary-hover"
+          style="transition: color 500ms ease"
+          href="/dashboard/courses/new"
+        >
+          Create new course
+        </a>
+      </p>
+    {/if}
+  </section>
+</main>
+
+<BecomeTeacher
+  show={becomeTeacherModalVisible}
+  on:close={() => (becomeTeacherModalVisible = false)}
+  on:done={({ detail: tcher }) => {
+    teacher = tcher;
+    becomeTeacherModalVisible = false;
+  }}
+/>
+
+<style lang="postcss">
+.grid-section {
+  @apply bg-primary rounded-lg px-6 py-4 m-4;
+  .heading {
+    @apply text-3xl text-center m-4;
+  }
+}
+</style>
