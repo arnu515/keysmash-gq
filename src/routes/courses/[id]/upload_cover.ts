1aa454eb59aadaef8f9cf47558131a65cc81e7ef
arnu515 <arnu5152@gmail.com>
2021-10-03 15:18:01 +0530

    prettier: format

--- src/routes/courses/[id]/upload_cover.ts
+++ src/routes/courses/[id]/upload_cover.ts
@@ -20,14 +20,17 @@ export const post: RequestHandler = async ({ body, locals, headers, params }) =>
   if (fError || !fData) {
     return { status: 401, body: { error: fError?.message || "Invalid token" } };
   }
-  const user = fData as unknown as {id: string};
+  const user = fData as unknown as { id: string };
 
   const { error: cError, data: cData } = await supabase
     .from("courses")
     .select()
     .eq("id", params.id);
   if (cError || !cData[0]) {
-    return { status: cError ? 400 : 404, body: { error: cError?.message || "Course not found" } };
+    return {
+      status: cError ? 400 : 404,
+      body: { error: cError?.message || "Course not found" }
+    };
   }
   const course = cData[0];
   if (course.teacher_id !== user.id) {
