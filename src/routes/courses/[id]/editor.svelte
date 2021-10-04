195f1f6815147cbc7f33a2992f23215d3bc9c8ca
arnu515 <arnu5152@gmail.com>
2021-10-04 14:36:24 +0530

    feat: add ability to edit and delete sections

--- src/routes/courses/[id]/editor.svelte
+++ src/routes/courses/[id]/editor.svelte
@@ -13,15 +13,101 @@ export const load: Load = ({
 <script lang="ts">
 import { goto } from "$app/navigation";
 import user from "$lib/stores/user";
-import { Course, getCourses, getTeacher, Teacher } from "$lib/supabase";
+import supabase, {
+  Course,
+  CourseLesson,
+  CourseSection,
+  getCourses,
+  getTeacher,
+  Teacher
+} from "$lib/supabase";
 import { onMount } from "svelte";
 import notifications from "$lib/stores/notifications";
+import EditOrCreateSectionModal from "$lib/components/EditOrCreateSectionModal.svelte";
 
 export let id: string;
 let course: Course;
 let loading = true;
 let teacher: Teacher;
 let sidebarOpen = true;
+let sections: Section[] = [];
+
+let editSection = false;
+let selectedSection: Section | {} = null;
+
+interface Section extends CourseSection {
+  lessons: CourseLesson[];
+}
+
+async function getSections() {
+  // Get all sections of a course with lessons
+  const { error, data } = await supabase
+    .from("course_sections")
+    .select(
+      `
+    id,
+    created_at,
+    name,
+    course_id,
+    description,
+    lessons:course_lessons(
+      *
+    )
+  `
+    )
+    .eq("course_id", course.id);
+  if (error) {
+    notifications.notify(error.message);
+    return [];
+  } else {
+    return data;
+  }
+}
+
+async function createOrEditSection(
+  section: Omit<CourseSection, "created_at" | "course_id">,
+  edit = false
+) {
+  console.log({ section, edit });
+  if (edit && !section.id) return;
+  const toInsert = {
+    name: section.name,
+    description: section.description,
+    id: edit ? section.id : undefined,
+    course_id: course.id
+  };
+  const { data, error } = await supabase
+    .from("course_sections")
+    .upsert(toInsert, { returning: "representation" });
+
+  if (error) {
+    notifications.notify(error.message);
+  } else {
+    const section = data[0];
+    if (!edit) sections = [...sections, { ...section, lessons: [] }];
+    else
+      sections = sections.map(i => {
+        if (i.id !== section.id) return i;
+        return { ...section, lessons: [] };
+      });
+
+    notifications.notify({
+      message: `${edit ? "Edited" : "Created"} section`,
+      type: "success"
+    });
+  }
+}
+
+async function deleteSection(sectionId: number) {
+  notifications.notify({ message: "Deleting section", type: "success" });
+  const { error } = await supabase.from("course_sections").delete().eq("id", sectionId);
+  if (error) {
+    notifications.notify(error.message);
+  } else {
+    sections = sections.filter(i => i.id !== sectionId);
+  }
+  notifications.notify({ message: "Deleted section", type: "success" });
+}
 
 onMount(async () => {
   if (!$user) {
@@ -53,6 +139,8 @@ onMount(async () => {
     notifications.notify("You don't have permission to do that!");
   }
 
+  sections = await getSections();
+
   loading = false;
 });
 </script>
@@ -66,10 +154,63 @@ onMount(async () => {
     {#if sidebarOpen}
       <aside class="sidebar">
         <h2 class="title">
-          Lectures <span class="close" on:click={() => (sidebarOpen = false)}
-            >&laquo;</span
+          Sections and Lessons <span
+            class="close"
+            on:click={() => (sidebarOpen = false)}>&laquo;</span
           >
         </h2>
+        {#each sections as section}
+          <div class="section">
+            <div
+              class="name"
+              on:click={() => {
+                selectedSection = section;
+                editSection = true;
+              }}
+            >
+              {section.name}
+            </div>
+            <ul class="lessons">
+              {#each section.lessons as lesson}
+                <li class="lesson">{lesson.title}</li>
+              {/each}
+              <li class="sidebar-button">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 4v16m8-8H4"
+                  />
+                </svg>
+                Add lesson
+              </li>
+            </ul>
+          </div>
+        {/each}
+        <div class="sidebar-button" on:click={() => (selectedSection = {})}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 4v16m8-8H4"
+            />
+          </svg>
+          Create new section
+        </div>
       </aside>
     {/if}
     <main class="lecture-area">
@@ -83,6 +224,27 @@ onMount(async () => {
   </section>
 {/if}
 
+<EditOrCreateSectionModal
+  show={!!selectedSection}
+  defaultValues={selectedSection || {}}
+  edit={editSection}
+  on:delete={({ detail: id }) => {
+    deleteSection(id);
+    selectedSection = undefined;
+    editSection = false;
+  }}
+  on:close={() => {
+    selectedSection = undefined;
+    editSection = false;
+  }}
+  on:done={e => {
+    const { section, edit } = e.detail;
+    createOrEditSection(section, edit);
+    selectedSection = undefined;
+    editSection = false;
+  }}
+/>
+
 <style lang="postcss">
 .main {
   @apply min-h-screen flex;
@@ -93,6 +255,32 @@ onMount(async () => {
       .close {
         @apply font-normal text-4xl hover:text-gray-200 cursor-pointer;
         transition: color 500ms ease;
+      }
+    }
+    .sidebar-button {
+      @apply bg-primary mt-2 cursor-pointer px-4 py-2 flex gap-4 items-center;
+      transition: 500ms filter ease;
+      &:hover {
+        filter: brightness(1.1);
+      }
+    }
+    .section {
+      @apply mt-2;
+      * {
+        cursor: pointer;
+        transition: 500ms filter ease;
+      }
+      *:hover {
+        filter: brightness(1.1);
+      }
+      .name {
+        @apply bg-primary-dark px-4 py-2;
+      }
+      .lessons {
+        @apply m-0 p-0 ml-2 list-none flex flex-col gap-2 justify-center;
+        .lesson {
+          @apply py-1 px-2;
+        }
       }
     }
   }
